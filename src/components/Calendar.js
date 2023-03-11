24e560ff0abf93f5f25f8b5adf0c2b4e1eb9cc51
Miguel Yax <mig_dj@hotmail.com>
2023-03-11 07:47:53 -0600

    add eslint config and dependencies

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,45 +1,52 @@
-import { parseDate, getMonthCalendar, dayNames } from '../utils'
-import { Container, Row, Col, Card } from 'react-bootstrap'
-import Nahual from './Nahual'
+import { Container, Row, Col } from 'react-bootstrap';
+import { parseDate, getMonthCalendar, dayNames } from '../utils';
+import Nahual from './Nahual';
 
 function Calendar({ date }) {
-    const dateConfig = parseDate(date);
-    console.log(dateConfig)
-    const {
-        monthName,
-        year,
-        weeks,
-    } = getMonthCalendar(dateConfig);
+  const dateConfig = parseDate(date);
+  console.log(dateConfig);
+  const {
+    monthName,
+    year,
+    weeks,
+  } = getMonthCalendar(dateConfig);
 
-
-    return (
-        <Container>
-            <div className='display-1'>{monthName} - {year}</div>
-            <Row>
-                <Col className="d-none d-lg-block" >
-                    <Row>
-                        {dayNames.map((name, key) => <Col key={key}>
-                            <div className='text-uppercase font-weight-bold'>{name}</div>
-                        </Col>)
-                        }
-                    </Row>
-                </Col>
-            </Row>
+  return (
+    <Container>
+      <div className="display-1">
+        {monthName}
+        {' '}
+        -
+        {' '}
+        {year}
+      </div>
+      <Row>
+        <Col className="d-none d-lg-block">
+          <Row>
+            {dayNames.map((name, key) => (
+              <Col key={key}>
+                <div className="text-uppercase font-weight-bold">{name}</div>
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
+      {
+        Object.entries(weeks).map(([key, week]) => (
+          <Row key={`week-${key}`}>
+            {' '}
             {
-                Object.entries(weeks).map(([key, week]) => {
-                    return (
-                        <Row key={`week-${key}`}> {
-                            week.map((day) =>
-                                <Col sm={12} lg={true} >
-                                    <Nahual {...day} />
-                                </Col>
-                            )}
-                        </Row>
-                    )
-                })
+              week.map((day) => (
+                <Col sm={12} lg>
+                  <Nahual {...day} weekDay={key} />
+                </Col>
+              ))
             }
-        </Container>
-    )
+          </Row>
+        ))
+      }
+    </Container>
+  );
 }
 
 export default Calendar;
--- src/components/Nahual.js
+++ src/components/Nahual.js
@@ -1,29 +1,35 @@
-
 import { dayIcons, nahualIcons } from './Days';
-import "./nahual.css"
+import { dayNames } from '../utils';
+import './nahual.css';
 import { Card } from 'react-bootstrap';
 
 const days = dayIcons();
-const nahuals = nahualIcons()
+const nahuals = nahualIcons();
 
-function Nahual({ day, nahual, nahualDay, isToday }) {
+function Nahual({
+    day, nahual, nahualDay, isToday, dayName,
+}) {
     if (!day) {
         return null;
     }
 
     return (
         <Card>
-            <Card.Header>{day}</Card.Header>
+            <Card.Header>
+                {day}
+                {' '}
+                <div className="d-none d-sm-block">{dayName}</div>
+            </Card.Header>
             <Card.Body>
-                <Card.Title>
-                </Card.Title>
+                <Card.Title />
                 <Card.Text>
+                    <p className="lead">{nahual}</p>
                     {days[nahualDay]}
                     {nahual && nahuals[nahual.replace(/'/g, '')]}
                 </Card.Text>
             </Card.Body>
         </Card>
-    )
+    );
 }
 
 export default Nahual;
